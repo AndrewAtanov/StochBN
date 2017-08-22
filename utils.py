17b54d902b188d57e3235ee25f8f7fb0b7dc5e77
Andrey Atanov <andrewatanov@yandex.ru>
2017-08-22 11:02:41 +0300

    add some stuff to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from models.stochbn import _MyBatchNorm
 
 
 class AccCounter:
@@ -16,3 +17,21 @@ class AccCounter:
     def flush(self):
         self.__n_objects = 0
         self.__sum = 0
+
+
+def set_collect(net, mode=True):
+    for m in net.modules():
+        if isinstance(m, _MyBatchNorm):
+            m.collect = mode
+
+def set_MyBN_strategy(net, mean_strategy='vanilla', var_strategy='vanilla'):
+    for m in net.modules():
+        if isinstance(m, _MyBatchNorm):
+            m.mean_strategy = mean_strategy
+            m.var_strategy = var_strategy
+
+
+def set_StochBN_train_mode(net, mode):
+    for m in net.modules():
+        if isinstance(m, _MyBatchNorm):
+            m.train_mode = mode
