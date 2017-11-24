Fix bug with loss calculation. Minor changes.

models/stochbn.py
@@ -93,6 +93,9 @@ class _MyBatchNorm(nn.Module):
         else:
             raise AssertionError("Don't change type of BN layer!")
 
+    def set_sample_policy(self, sample_policy='one'):
+        self.__sample_policy = sample_policy
+
     def reset_parameters(self):
         self.running_mean.zero_()
         self.running_var.fill_(1)
@@ -142,14 +145,6 @@ class _MyBatchNorm(nn.Module):
 
         self.running_mean.copy_(self.running_m)
         self.running_var.copy_(torch.exp(self.running_logvar))
-        # self.running_var = (1 - self.stats_momentum) * self.running_var + self.stats_momentum * self.cur_var
-
-
-        # s = torch.log(self.running_var) - self.running_logvar
-        # # TODO: delete assert
-        # assert np.all(s.cpu().numpy() >= 0)
-        # self.running_var_shape.copy_((3 - s + torch.sqrt((s - 3) ** 2 + 24 * s)) / 12. / s)
-        # self.running_var_scale.copy_(self.running_var / self.running_var_shape)
 
     def flush_stats(self):
         self.sum_m.zero_()

train.py
@@ -41,7 +41,7 @@ parser.add_argument('--no-augmentation', dest='augmentation', action='store_fals
 parser.set_defaults(augmentation=True)
 parser.add_argument('--model', '-m', default='ResNet18', help='Model')
 parser.add_argument('--k', '-k', default=1, type=float, help='Model size for VGG')
-parser.add_argument('--dropout', type=float, default=None)
+parser.add_argument('--dropout', type=float, nargs='+', default=None)
 parser.add_argument('--weight_decay', type=float, default=0.)
 parser.add_argument('--decay', default=None, type=float, help='Decay rate')
 parser.add_argument('--finetune_bn', action='store_true')
@@ -175,6 +175,7 @@ def lr_exponential(epoch):
 def sample_weight_linear(epoch):
     return float(1. - max(0, ((1. - args.sample_w_init) * np.minimum((args.sample_stats_from - epoch) * 1. / args.sample_w_iter + 1, 1.))))
 
+
 best_test_acc = 0
 counter = AccCounter()
 
@@ -227,7 +228,7 @@ for epoch in range(args.epochs):
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        training_loss += loss.cpu().data.numpy()[0]
+        training_loss += loss.cpu().data.numpy()[0] * float(args.bs)
 
         if args.log_grads:
             for name, param in net.named_parameters():
@@ -257,7 +258,7 @@ for epoch in range(args.epochs):
         inputs, labels = Variable(inputs.cuda(async=True)), Variable(labels.cuda(async=True))
         outputs = net(inputs)
         loss = criterion(outputs, labels)
-        test_loss += to_np(loss)
+        test_loss += to_np(loss) * float(args.bs)
         counter.add(outputs.data.cpu().numpy(), labels.data.cpu().numpy())
 
     writer.add_scalar('sample_weight', sample_w, epoch + 1)
@@ -271,22 +272,18 @@ for epoch in range(args.epochs):
         'state_dict': net.module.state_dict() if use_cuda else net.state_dict(),
         'test_accuracy': counter.acc(),
         'optimizer': optimizer.state_dict(),
-        'net': net.module if use_cuda else net,
+        # 'net': net.module if use_cuda else net,
         'name': args.model,
         # TODO: not flexible
         'model_args': model_args,
         'script_args': vars(args)
     }, best_test_acc < counter.acc())
 
-    writer.add_scalars('log-loss', {
-        'train': float(np.log(training_loss)),
-        'test': float(np.log(test_loss))
-    }, global_step=epoch)
+    writer.add_scalar('log-loss/train', float(np.log(training_loss)), global_step=epoch)
+    writer.add_scalar('log-loss/test', float(np.log(test_loss)), global_step=epoch)
 
-    writer.add_scalars('metrics/accuracy', {
-        'train': float(train_acc),
-        'test': float(counter.acc()),
-    }, global_step=epoch)
+    writer.add_scalar('accuracy/train', float(train_acc), global_step=epoch)
+    writer.add_scalar('accuracy/test', float(counter.acc()), global_step=epoch)
 
     writer.export_scalars_to_json(os.path.join(args.log_dir, 'all_scalars.json'))
 