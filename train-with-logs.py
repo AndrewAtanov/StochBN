8c1befa0f140cca11e465e05fb77ea0bd700b595
andrew <andrewatanov@yandex.ru>
2017-12-05 23:30:37 +0300

    delete unused imports

--- train-with-logs.py
+++ train-with-logs.py
@@ -3,14 +3,12 @@ from __future__ import print_function
 
 import torch
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
 from utils import *
 
 import numpy as np
 
 import torchvision
 import torchvision.transforms as transforms
-from torch.nn.parallel import DataParallel
 
 import os
 import argparse
@@ -20,9 +18,8 @@ from models import *
 from models.stochbn import _MyBatchNorm
 from torch.autograd import Variable
 
-import shutil, pickle
+import shutil
 from time import time
-import importlib
 from tensorboardX import SummaryWriter
 
 parser = argparse.ArgumentParser(description='Net training')
