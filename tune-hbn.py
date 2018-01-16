c8a5f4d5ec186dfc30f2703d203538e72b69bb89
andrew <andrewatanov@yandex.ru>
2018-01-17 00:24:25 +0300

    minor upd

--- tune-hbn.py
+++ tune-hbn.py
@@ -9,11 +9,17 @@ from models.stochbn import _MyBatchNorm
 
 parser = argparse.ArgumentParser(description='Net training')
 parser.add_argument('--model')
+parser.add_argument('--new_model', default=None)
 parser.add_argument('--epochs', type=int)
 parser.add_argument('--data')
 parser.add_argument('--sample_policy', default='one')
+parser.add_argument('--stats_strategy', default='sample')
 parser.add_argument('--bs', default='auto')
+parser.add_argument('--augmentation', dest='augmentation', action='store_true')
+parser.add_argument('--no-augmentation', dest='augmentation', action='store_false')
+parser.set_defaults(augmentation=True)
 parser.add_argument('--n_sbn', default=None, type=int)
+parser.add_argument('--stats_momentum', default=0.1, type=float)
 args = parser.parse_args()
 
 net = utils.load_model(args.model)
@@ -21,11 +27,12 @@ net = utils.load_model(args.model)
 ckpt = torch.load(args.model)
 train_bs = ckpt['script_args']['bs'] if args.bs == 'auto' else int(args.bs)
 
-trainloader, _ = utils.get_dataloader(data=args.data, train_bs=train_bs)
+trainloader, _ = utils.get_dataloader(data=args.data, train_bs=train_bs, augmentation=args.augmentation)
 
 utils.set_bn_mode(net, 'StochBN', sample_policy=args.sample_policy)
 net.train()
-utils.set_MyBN_strategy(net, mean_strategy='sample', var_strategy='sample')
+utils.set_bn_params(net, stats_momentum=args.stats_momentum)
+utils.set_MyBN_strategy(net, mean_strategy=args.stats_strategy, var_strategy=args.stats_strategy)
 
 if args.n_sbn:
     bns = [m for m in net.modules() if isinstance(m, _MyBatchNorm)]
@@ -40,7 +47,9 @@ for _ in tqdm(range(args.epochs)):
 ckpt['state_dict'] = net.module.state_dict()
 ckpt['tune_epoch'] = args.epochs
 
-fname = 'tuned_' + os.path.basename(args.model)
+fname = args.new_model
+if fname is None:
+    fname = 'tuned_' + os.path.basename(args.model)
 
 if args.bs != 'auto':
     fname += '_{}'.format(args.bs)
